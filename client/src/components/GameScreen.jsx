4aaf9fb962cfdc65e6a912f9793551ccac24d522
vpatel89 <virenpatel89@gmail.com>
2022-08-10 19:32:07 -0400

    added game image and options to select from

--- client/src/components/GameScreen.jsx
+++ client/src/components/GameScreen.jsx
@@ -5,6 +5,24 @@ const GameScreen = (props) => {
   return (
     <div className='game'>
       <h1>Game is about to begin</h1>
+
+      <div className='triviaSection'>
+        <div id='timer'>Timer</div>
+        <div id='score'>Score</div>
+
+        <div className='imageDiv'>
+          <img id='gameImage' src={ props.gameImage } alt="..." />
+        </div>
+
+        <h2>Guess the game based on the Screenshot</h2>
+        <div className='answerOptions'>
+          <button onClick={() => { props.checkAnswer() }}>{ props.option1 }</button>
+          <button onClick={() => { props.checkAnswer() }}>{ props.option2 }</button>
+          <button onClick={() => { props.checkAnswer() }}>{ props.option3 }</button>
+          <button onClick={() => { props.checkAnswer() }}>{ props.option4 }</button>
+        </div>
+      </div>
+
     </div>
   )
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,20 +11,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: ''
+      username: '',
+      currentScore: 0,
+      data: [],
+      gameImage: '',
+      answer: '',
+      option1: '',
+      option2: '',
+      option3: '',
+      option4: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.checkAnswer = this.checkAnswer.bind(this);
   }
 
   componentDidMount() {
     this.checkUsername();
-    // axios.get(`https://api.rawg.io/api/games/12020/screenshots?key=${API_KEY}`)
-    // .then((asdf) => {
-    //   console.log(asdf);
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // })
+
   }
 
   // axios.get(`https://api.rawg.io/api/games/4200/screenshots?key=${API_KEY}`)
@@ -34,7 +37,7 @@ class App extends React.Component {
     let value = event.target.value;
     this.setState({
       [event.target.name]: value
-      }, () => { this.checkUsername(this.state.username) }
+      }, () => { this.checkUsername() }
     );
   }
 
@@ -51,6 +54,75 @@ class App extends React.Component {
   beginGame = () => {
     document.getElementsByClassName("start")[0].style.display = 'none';
     document.getElementsByClassName("game")[0].style.display = 'block';
+    this.generateQuestion();
+  }
+
+
+  generateQuestion = () => {
+    let pageNum = Math.floor(Math.random() * 61);
+    let resultNum = Math.floor(Math.random() * 20);
+    axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=${pageNum}&ordering=-metacritic`)
+    .then((games) => {
+      let imageCount = games.data.results[resultNum].short_screenshots.length;
+      let randomImageIndex = Math.floor(Math.random() * imageCount);
+
+      this.setState({
+        gameImage: games.data.results[resultNum].short_screenshots[randomImageIndex].image,
+        answer: games.data.results[resultNum].name
+        }, () => { this.setOptions(games.data.results) }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+
+  setOptions = (gamesList) => {
+    let otherGameTitles = [];
+    gamesList.forEach((game) => {
+      if (game.name !== this.state.answer) {
+        otherGameTitles.push(game.name);
+      }
+    })
+
+    let options = [];
+    options.push(this.state.answer);
+    while (options.length < 4) {
+      let randomIndex = Math.floor(Math.random() * otherGameTitles.length);
+      options.push(otherGameTitles[randomIndex]);
+      otherGameTitles.splice(randomIndex, 1);
+    }
+
+    for (let i = options.length - 1; i > 0; i--) {
+      let randomInd = Math.floor(Math.random() * (i + 1));
+      let temp = options[i];
+      options[i] = options[randomInd];
+      options[randomInd] = temp;
+    }
+
+    this.setState({
+      option1: options[0],
+      option2: options[1],
+      option3: options[2],
+      option4: options[3]
+    });
+  }
+
+
+  checkAnswer = () => {
+    console.log()
+    if (event.target.innerText === this.state.answer) {
+      this.setState({
+        currentScore: (this.state.currentScore + 100)
+      })
+    } else {
+      this.setState({
+        currentScore: (this.state.currentScore - 100)
+      })
+    }
+
+    this.generateQuestion();
   }
 
 
@@ -58,7 +130,7 @@ class App extends React.Component {
     return (
       <>
         <StartScreen handleChange={this.handleChange} beginGame={this.beginGame} />
-        <GameScreen />
+        <GameScreen checkAnswer={this.checkAnswer} gameImage={this.state.gameImage} option1={this.state.option1} option2={this.state.option2} option3={this.state.option3} option4={this.state.option4} />
       </>
     )
   }
